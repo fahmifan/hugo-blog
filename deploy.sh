a85d28d27e315727513c8ab4427093b9d5a73316
fahmifan <fahmifan909@gmail.com>
2018-07-19 15:50:22 +0700

    update script

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,19 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t cocoa-eh # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo --config config.toml # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
+
+# hapus symbolic link img
+rm -rf static/img
+
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="rebuilding fahmifan.github.io `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
@@ -25,3 +29,4 @@ cd ..
 
 git add public/
 git commit -m "rebuild site `date`"
+git push origin master
